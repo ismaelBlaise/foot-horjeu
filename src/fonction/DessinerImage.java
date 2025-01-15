26d45d64fe6709f71a70f6813b8ba547438b2b10
ismaelBlaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-01-15 16:35:18 +0300

    ok

--- src/fonction/DessinerImage.java
+++ src/fonction/DessinerImage.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class DessinerImage {
 
     public static void dessinerSurImage(Mat image, List<Joueur> joueurs,List<Joueur> joueursOposant,double tailleTerrain, Point positionBallon, String couleur, Joueur joueurProcheBallon) {
-        Scalar couleurTexte = couleur.equals(JoueurCouleur.ROUGE) ? new Scalar(0, 0, 255) : new Scalar(255, 0, 0);
+        // Scalar couleurTexte = couleur.equals(JoueurCouleur.ROUGE) ? new Scalar(0, 0, 255) : new Scalar(255, 0, 0);
         Scalar couleurRect = couleur.equals(JoueurCouleur.ROUGE) ? new Scalar(0, 255, 255) : new Scalar(255, 255, 0);
 
          List<Joueur> joueursAll=new ArrayList<>();
@@ -28,8 +28,8 @@ public class DessinerImage {
 
             Imgproc.rectangle(image, joueur.getDimension().tl(), joueur.getDimension().br(), couleurRect, 2);
 
-            Imgproc.putText(image, statut, new Point(position.x - 15, position.y - 15),
-                    Imgproc.FONT_HERSHEY_SIMPLEX, 0.7, couleurTexte, 2);
+            Imgproc.putText(image, statut, new Point(position.x - 15, position.y - 20),
+                    Imgproc.FONT_HERSHEY_SIMPLEX, 0.7, new Scalar(0, 0, 0), 2);
 
             if (joueur.equals(joueurProcheBallon)) {
                         Imgproc.rectangle(image, joueur.getDimension().tl(), joueur.getDimension().br(),  new Scalar(0, 255, 0), 2);
